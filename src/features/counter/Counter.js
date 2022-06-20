df5f8be9628bdf685d46db715c72d3c308c025cf
Aimal Amiri <meaimal@hotmail.com>
2022-06-20 17:21:20 +0430

    Configure the react-router-dom

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -8,7 +8,6 @@ import {
   incrementIfOdd,
   selectCount,
 } from './counterSlice';
-import styles from './Counter.module.css';
 
 export default function Counter() {
   const count = useSelector(selectCount);
@@ -19,18 +18,16 @@ export default function Counter() {
 
   return (
     <div>
-      <div className={styles.row}>
+      <div>
         <button
-          className={styles.button}
           type="button"
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
-        <span className={styles.value}>{count}</span>
+        <span>{count}</span>
         <button
-          className={styles.button}
           type="button"
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
@@ -38,29 +35,25 @@ export default function Counter() {
           +
         </button>
       </div>
-      <div className={styles.row}>
+      <div>
         <input
-          className={styles.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
-          className={styles.button}
           type="button"
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
         </button>
         <button
-          className={styles.asyncButton}
           type="button"
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
         </button>
         <button
-          className={styles.button}
           type="button"
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
