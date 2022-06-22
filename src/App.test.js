87299f475a1fe32032c1757eeee14e7ada7a9b6d
Aimal Amiri <meaimal@hotmail.com>
2022-06-22 17:17:08 +0430

    RCP-7: Fix the error in App.test.js file

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import store from './Redux/store';
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <MemoryRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MemoryRouter>,
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Kabul/i)).toBeInTheDocument();
 });
