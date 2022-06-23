1f1c3b6d83b56b24a5a709d8bfbf0a412cdcee70
Aimal Amiri <meaimal@hotmail.com>
2022-06-23 13:39:20 +0430

    RCP-8: Design the home page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,11 +5,11 @@ import microphone from '../assets/svg/microphone.svg';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center bg-gradient-to-br from-pink-400 to-pink-700 py-2 px-3">
+    <nav className="flex justify-between items-center bg-pink-600 py-2 px-3">
       <Link to="/" className="text-blue-600 hover:text-blue-500">
         <img src={leftArrow} alt="icon" className="h-4" />
       </Link>
-      <span className="text-white font-lato">Weather Condition</span>
+      <span className="text-white font-lato">Select a City</span>
       <div className="flex gap-4">
         <img src={microphone} alt="icon" className="h-4" />
         <img src={settingsGear} alt="icon" className="h-4" />
